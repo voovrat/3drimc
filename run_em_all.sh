960bc3027009f2f4899a2112cf0b387783f01491
Volodymyr Sergiievskyi <vsergiie@agnelli.theochem.uni-due.de>
2015-12-01 14:40:01 +0100

    3drimc_rdf and so on...

---
+++ run_em_all.sh
@@ -0,0 +1,6 @@
+3drimc_run_on_host hosts/localhost.proc1 states rismmol_files 10000 40 >/dev/null 2>/dev/null &
+3drimc_run_on_host hosts/localhost.proc2 states rismmol_files 10000 40 >/dev/null 2>/dev/null &
+3drimc_run_on_host hosts/localhost.proc3 states rismmol_files 10000 40 >/dev/null 2>/dev/null &
+3drimc_run_on_host hosts/localhost.proc4 states rismmol_files 10000 40 >/dev/null 2>/dev/null &
+3drimc_run_on_host hosts/localhost.proc5 states rismmol_files 10000 40 >/dev/null 2>/dev/null &
+3drimc_run_on_host hosts/localhost.proc6 states rismmol_files 10000 40 >/dev/null 2>/dev/null &
